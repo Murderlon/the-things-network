abb0011b8e75fdb1631d10a0cb3eaf67961fd9fb
Murderlon <merlijn@soverin.net>
2018-11-26 17:34:05 +0100

    remove unused plugins

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -2,32 +2,5 @@ module.exports = {
   siteMetadata: {
     title: 'The Things Network'
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-styled-components',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        // This path is relative to the root of the site.
-        icon: 'src/images/gatsby-icon.png'
-      }
-    }
-    // https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ]
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components']
 }
