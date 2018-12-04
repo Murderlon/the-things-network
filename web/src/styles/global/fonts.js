1b97c4fe6b877ff127808c1fd6246b5ce93d0a5a
Murderlon <merlijn@soverin.net>
2018-12-04 16:37:37 +0100

    use `typeface-fira-sans` package in fonts

--- web/src/styles/global/fonts.js
+++ web/src/styles/global/fonts.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 import { css } from 'styled-components'
+import 'typeface-fira-sans'
 
 export default css`
   @font-face {
@@ -28,41 +29,5 @@ export default css`
         format('woff2'),
       url('//cloud.typenetwork.com/projectLicenseWeb/14696/fontfile/woff/?79035cbd97fb22a3ff9629954f48cb2f395ec62b')
         format('woff');
-  }
-
-  @font-face {
-    font-family: 'Fira Sans';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: local('Fira Sans Regular'), local('FiraSans-Regular'),
-      url(https://fonts.gstatic.com/s/firasans/v8/va9E4kDNxMZdWfMOD5Vvl4jLazX3dA.woff2)
-        format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
-      U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-
-  @font-face {
-    font-family: 'Fira Sans';
-    font-style: normal;
-    font-weight: 500;
-    font-display: swap;
-    src: local('Fira Sans Medium'), local('FiraSans-Medium'),
-      url(https://fonts.gstatic.com/s/firasans/v8/va9B4kDNxMZdWfMOD5VnZKveRhf6Xl7Glw.woff2)
-        format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
-      U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-  }
-
-  @font-face {
-    font-family: 'Fira Sans';
-    font-style: normal;
-    font-weight: 700;
-    font-display: swap;
-    src: local('Fira Sans Bold'), local('FiraSans-Bold'),
-      url(https://fonts.gstatic.com/s/firasans/v8/va9B4kDNxMZdWfMOD5VnLK3eRhf6Xl7Glw.woff2)
-        format('woff2');
-    unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
-      U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 `
