2f276d4a13a99ad7889f9bd6287ec550837f8b9b
Murderlon <merlijn@soverin.net>
2019-01-18 20:30:55 +0100

    update AnimatedGrid

--- web/src/components/AnimatedGrid.js
+++ web/src/components/AnimatedGrid.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import { select, selectAll } from 'd3-selection'
 
-import GridSVG from 'assets/transparent-grid.svg'
+import grid18 from 'assets/transparent-grid-18.svg'
 import variables from '../styles/variables'
 
-let TransparentGrid = styled(GridSVG)`
+let TransparentGrid = styled(grid18)`
   position: absolute;
   z-index: -10;
   width: 100%;
@@ -13,37 +13,96 @@ let TransparentGrid = styled(GridSVG)`
   grid-column: 1 / 19;
 `
 
+// Beware, is an ugly
 class AnimatedGrid extends Component {
-  componentDidMount() {
-    let rects = select('#transparent-grid').selectAll('rect[y="480"]')
-      ._groups[0]
-
-    this.timeout = setTimeout(() => this.animateCubes(rects), 1000)
+  state = {
+    hasLargeScreen: null
   }
 
-  animateCubes = rects => {
+  componentDidMount() {
+    this.mq = window.matchMedia(`(min-width: 90rem)`)
+
+    this.setState({ hasLargeScreen: this.mq.matches })
+    this.mq.addListener(this.update)
+
+    if (this.mq.matches) {
+      this.setupAnimatedGrid()
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.hasLargeScreen === null) return
+    let { hasLargeScreen } = this.state
+    console.log('didupdate')
+
+    if (hasLargeScreen && !prevState.hasLargeScreen) {
+      console.log('resstart')
+      this.setupAnimatedGrid()
+    }
+    if (!hasLargeScreen && prevState.hasLargeScreen) {
+      this.svg.selectAll('rect').style('fill-opacity', 0)
+      this.removeListeners()
+    }
+  }
+
+  componentWillUnmount() {
+    this.removeListeners()
+  }
+
+  update = ({ matches }) => {
+    this.setState({ hasLargeScreen: matches })
+  }
+
+  setupAnimatedGrid() {
+    this.svg = select('#transparent-grid')
+
+    this.rows = Array.from(
+      new Set(
+        Array.from(this.svg.selectAll('rect')._groups[0]).map(rect =>
+          rect.getAttribute('y')
+        )
+      )
+    ).filter(Boolean)
+
+    this.timeoutOne = setTimeout(() => this.animateCubes('intervalOne'), 1000)
+    this.timeoutTwo = setTimeout(() => this.animateCubes('intervalTwo'), 2000)
+  }
+
+  random = () => {
+    let randomIndex = Math.floor(Math.random() * (this.rows.length - 1 + 1) + 1)
+    return Array.from(
+      this.svg.selectAll(`rect[y="${this.rows[randomIndex]}"]`)._groups[0]
+    ).reverse()
+  }
+
+  animateCubes = name => {
     let index = 0
     let slice = 0
+    let rects = this.random()
 
-    this.interval = setInterval(() => {
+    this[name] = setInterval(() => {
       if (index + 7 === 24) {
         index = 0
         slice = 0
         selectAll(rects).style('fill-opacity', 0)
+        rects = this.random()
       } else {
-        selectAll(
-          Array.from(rects).slice(index, index + (slice < 7 ? slice++ : slice))
-        )
+        if (slice < 7) slice++
+        selectAll(rects.slice(index, index + slice))
           .style('fill', variables.green)
           .style('fill-opacity', (_, i) => i / 10)
       }
       index++
-    }, 150)
+    }, 100)
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval)
-    clearTimeout(this.timeout)
+  removeListeners = () => {
+    clearInterval(this.intervalOne)
+    clearInterval(this.intervalTwo)
+    clearTimeout(this.timeoutOne)
+    clearTimeout(this.timeoutTwo)
+
+    this.mq.removeListener(this.update)
   }
 
   render() {
